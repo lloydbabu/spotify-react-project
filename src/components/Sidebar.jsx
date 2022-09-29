e8776a745bca705e561150740b8356ec2ba2a57d
Lloyd P Babu <lloyd@thinkrisk.ai>
2022-09-30 00:03:35 +0530

    eslint fixes in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,26 +18,25 @@ const NavLinks = ({ handleClick }) => (
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-   
-  return(
-  <>
-    <div className="hidden md:flex flex-col w=[240px] py-10 px-4 bg-[#191624]">
-    <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-    <NavLinks />
-    </div>   
+  return (
+    <>
+      <div className="hidden md:flex flex-col w=[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
 
-    <div className="absolute md:hidden block top-6 right-3">
-      {
+      <div className="absolute md:hidden block top-6 right-3">
+        {
         mobileMenuOpen ? <RiCloseLine onClick={() => { setMobileMenuOpen(false); }} className="w-6 h-6 text-white mr-2" /> : <HiOutlineMenu onClick={() => { setMobileMenuOpen(true); }} className="w-6 h-6 text-white mr-2" />
       }
-    </div>
+      </div>
 
-    <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-      <NavLinks handleClick={() => setMobileMenuOpen(false)} />
-    </div>
-  </>
-);
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
